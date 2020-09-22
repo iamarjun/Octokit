c3065635eb57d2a969c48650a71653fd5df05544
iamarjun <arjunmanoj1995@gmail.com>
2020-09-22 22:17:04 +0530

    setup network module

---
+++ app/src/main/java/com/arjun/octokit/GithubApi.kt
@@ -0,0 +1,3 @@
+package com.arjun.octokit
+
+interface GithubApi
---
+++ app/src/main/java/com/arjun/octokit/di/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.arjun.octokit.di
+
+import androidx.viewbinding.BuildConfig
+import com.arjun.octokit.GithubApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideRetrofitBuilder(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+
+    @Provides
+    fun provideRestApi(retrofit: Retrofit): GithubApi {
+        return retrofit.create(GithubApi::class.java)
+    }
+}
